5f09c2a406deae6d4a632a2c270ade9911b59265
Jose Jimenez <168050766+Unknow808@users.noreply.github.com>
2025-07-04 16:08:17 +0200

    Actualiza favicon y iconos de la web

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"dd93de6fb1776398bf586cbd477deade1391c7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "715378527"
+    serviceWorkerVersion: "2443294531"
   }
 });
